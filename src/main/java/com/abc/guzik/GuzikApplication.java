9f8f9347b1aedb1f88ec938a10d7f802e4bf6bea
Raman Baranau <35633313+raman-baranau@users.noreply.github.com>
2024-03-01 05:02:10 +0300

    Initial setup

---
+++ src/main/java/com/abc/guzik/GuzikApplication.java
@@ -0,0 +1,13 @@
+package com.abc.guzik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuzikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuzikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abc/guzik/GuzikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.guzik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuzikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
